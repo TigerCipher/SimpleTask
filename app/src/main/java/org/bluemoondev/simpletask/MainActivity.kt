f8910dc77358ea84ec5e6f7ed0e5098a3c5b01e0
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2023-12-17 15:36:24 -0600

    Just playing around learning how to use the compose UI

--- app/src/main/java/org/bluemoondev/simpletask/MainActivity.kt
+++ app/src/main/java/org/bluemoondev/simpletask/MainActivity.kt
@@ -3,13 +3,23 @@ package org.bluemoondev.simpletask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.bluemoondev.simpletask.ui.theme.SimpleTaskTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +29,7 @@ class MainActivity : ComponentActivity() {
             SimpleTaskTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("User", "Matt")
                 }
             }
         }
@@ -27,17 +37,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Greeting(name: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hi there $name!",
+//            modifier = modifier.padding(24.dp),
+            fontSize = 32.sp,
+            lineHeight = 48.sp,
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = "from $from",
+            modifier = modifier.padding(16.dp).align(Alignment.End),
+            fontSize = 24.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SimpleTaskTheme {
-        Greeting("Android")
+        Greeting("Matt", "Sara")
     }
 }
